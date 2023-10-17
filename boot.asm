128493ebd6637793cd519209e8e9651c4145406c
ahmad <ahmadsameh660@gmail.com>
2023-10-17 11:25:41 +0300

    added segmentation to the bootloader to avoid errors

--- boot.asm
+++ boot.asm
@@ -1,12 +1,22 @@
 ; the boot loader is what loads the kernel into memory
 
-        org     0x7c00   ; the adress where the bios loads the bootloader
+        org     0       
         bits    16    ; use 16 bit architecture, because during the boot process, real mode is on which only works on 16 bits
-
+        jmp     0x7c0:start ; make code segment to 0x7c0
 start:
+        cli ; clear interrupt flags, to disable interrupts to change segment registers so hardware interrupts do not interfere
+        ; change data segments
+        mov     ax, 0x7c0
+        mov     ds, ax    ; must copy the segment first to ax then ds, no direct addressing on ds
+        mov     es, ax    ; gotta change these data segments and not count on bios
+        ; change stack segment
+        mov     ax, 0x00
+        mov     ss, ax    
+        mov     sp, 0x7c00 ; set stack pointer to 0x7c00
+        sti ; enable interrupts 
         mov     si, message ; a pointer to the start of the message we want to print
         call    print  ; print the message 
-        jmp    $  ; jump to the same line
+        jmp     $  ; jump to the same line
 
 print:  ; function to print a string
         mov     bx, 0  ; bh for page number and bl for foreground color
