1519d026572e4e29f44a63cf637382466f7eec4f
AhmadSameh <ahmadsameh660@gmail.com>
2023-11-24 01:34:36 +0200

    protected mode: implemented fat16 structures

--- src/fs/fat/fat16.h
+++ src/fs/fat/fat16.h
@@ -1,8 +1,11 @@
 #ifndef FAT16_H
 #define FAT16_H
 
+#include <stdint.h>
 #include "../file.h"
 #include "../../status.h"
+#include "../../disk/disk.h"
+#include "../../disk/streamer.h"
 #include "../../string/string.h"
 
 struct filesystem* fat16_init(void);
